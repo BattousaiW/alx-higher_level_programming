93d6d956e39483bb4f95d5829e1e6d436a9e4a02
BattousaiW <Brandonwee08@gmail.com>
2024-04-15 13:55:54 +0200

    SQL Queries update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,3 @@
 -- Creates the user user_0d_1 with all privileges.
-CREATE USER IF NOT EXISTS 'user_01_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
--- Grant all privileges to user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
--- flush all privileges
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
